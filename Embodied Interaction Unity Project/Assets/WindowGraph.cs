04b16326e4d9fbe770d5af564294dce6bfa120c2
EoinRaff <eoinrafferty@gmail.com>
2019-04-29 13:35:53 +0200

    working simple graph

--- Embodied Interaction Unity Project/Assets/WindowGraph.cs
+++ Embodied Interaction Unity Project/Assets/WindowGraph.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using CodeMonkey.Utils;
 
 public class WindowGraph : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class WindowGraph : MonoBehaviour
     [SerializeField]
     private Sprite circleSprite;
 
-    private void createCircle(Vector2 anchoredPosition)
+    private void Start()
+    {
+        List<float> values = new List<float> { 33f, 13f, 75f, 10f, 15f, 16f, 17f, 26f, 37f, 27f, 38f, 87f, 75f, 50 };
+        ShowGraph(values);
+    }
+    private GameObject createCircle(Vector2 anchoredPosition)
     {
         GameObject gameObject = new GameObject("circle", typeof(Image));
         gameObject.transform.SetParent(graphContainer, false);
@@ -20,13 +26,42 @@ public class WindowGraph : MonoBehaviour
         rectTransform.sizeDelta = new Vector2(11, 11);
         rectTransform.anchorMin = Vector2.zero;
         rectTransform.anchorMax = Vector2.zero;
+        return gameObject;
     }
 
     private void ShowGraph(List<float> values)
     {
-        foreach (float n in values)
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f;
+        float xSize = 50f;
+        GameObject previousCircle = null;
+        for (int i = 0; i < values.Count; i++)
         {
-
+            float xPosition = i * xSize;
+            float yPosition = (values[i] / yMaximum) * graphHeight;
+            GameObject circle = createCircle(new Vector2(xPosition, yPosition));
+            if (previousCircle != null)
+            {
+                CreateDotConnection(previousCircle.GetComponent<RectTransform>().anchoredPosition, circle.GetComponent<RectTransform>().anchoredPosition);
+            }
+            previousCircle = circle;
         }
+    }
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("Dot Connection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 direction = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.zero;
+        rectTransform.sizeDelta = new Vector2(distance, 3);
+        rectTransform.anchoredPosition = dotPositionA + direction * distance * 0.5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(direction));
+
+
+
     }
 }
