0ce5c413b066487f661a81fa25232aa668de61f4
EoinRaff <eoinrafferty@gmail.com>
2019-04-29 12:51:27 +0200

    started graph functionality

--- Embodied Interaction Unity Project/Assets/LabanDescriptors.cs
+++ Embodied Interaction Unity Project/Assets/LabanDescriptors.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
 using Windows.Kinect;
 using Joint = Windows.Kinect.Joint;
 
---
+++ Embodied Interaction Unity Project/Assets/WindowGraph.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowGraph : MonoBehaviour
+{
+
+    public RectTransform graphContainer;
+    [SerializeField]
+    private Sprite circleSprite;
+
+    private void createCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.zero;
+    }
+
+    private void ShowGraph(List<float> values)
+    {
+        foreach (float n in values)
+        {
+
+        }
+    }
+}
