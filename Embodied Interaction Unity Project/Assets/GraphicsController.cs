c3217a1803e04e42d9665917d9902a7a8eb19226
EoinRaff <eoinrafferty@gmail.com>
2019-05-29 10:23:53 +0200

    particle size based on Time

---
+++ Embodied Interaction Unity Project/Assets/GraphicsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsController : Singleton<GraphicsController>
+{
+    public LabanDescriptors laban;
+
+    public float Weight { get; private set; }
+    public float Time { get; private set; }
+    public float Flow { get; private set; }
+    public float Space { get; private set; }
+
+    void Update()
+    {
+        Weight = laban.WeightEffort;
+        Time = laban.TimeEffort;
+        Flow = laban.FlowEffort;
+        Space = laban.SpaceEffort;
+    }
+}
---
+++ Embodied Interaction Unity Project/Assets/StarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    private Vector3 baseScale;
+    void Start()
+    {
+        baseScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GraphicsController.Instance.Time > 2f)
+        {
+            ChangeSize(GraphicsController.Instance.Time / 10f);
+        }
+        //ReturnToBaseScale();
+    }
+
+    private void ReturnToBaseScale()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, baseScale, Time.deltaTime);
+    }
+
+    public void ChangeSize(float TimeEffort)
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, baseScale * TimeEffort, Time.deltaTime * TimeEffort);
+    }
+}
